f08d1a05063e5860af76f2c5f56d05399e2640aa
Terry M. Poulin <BigBoss1964@gmail.com>
2011-06-18 18:29:39 +0000

    Very simple echo loop

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 
 #include <cstdlib>
 
+#include <iostream>
+#include <string>
+
 int
 main(int argc, char *argv[])
 {
+    std::string input;
+
+    std::cout << "$ ";
+    while (std::getline(std::cin, input)) {
+        std::cout << "> '" << input << "'" << std::endl;
+        std::cout << "$ ";
+    }
+
     std::exit(EXIT_SUCCESS);
 }
 
